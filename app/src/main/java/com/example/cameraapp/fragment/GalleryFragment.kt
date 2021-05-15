c8c8a4234aa59a456ff4943d22a52e972610b919
anjali1361 <anjalikumari13617@gmail.com>
2021-05-15 10:16:53 +0530

    mediapager code exist in this branch

--- app/src/main/java/com/example/cameraapp/fragment/GalleryFragment.kt
+++ app/src/main/java/com/example/cameraapp/fragment/GalleryFragment.kt
@@ -1,5 +1,8 @@
 package com.example.cameraapp.fragment
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.media.MediaScannerConnection
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -9,21 +12,26 @@ import java.io.File
 import android.os.Build
 import android.util.Log
 import android.view.LayoutInflater
+import android.webkit.MimeTypeMap
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.FileProvider
 import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.viewpager.widget.ViewPager
 import com.blackbox.imageutils.models.ImageItem
+import com.example.cameraapp.BuildConfig
 import com.example.cameraapp.R
 import com.example.cameraapp.adapters.ImageAdapter
 import com.example.cameraapp.utils.ImageCrypter
 import com.example.cameraapp.utils.padWithDisplayCutout
+import com.example.cameraapp.utils.showImmersive
 import io.reactivex.rxkotlin.subscribeBy
 import java.util.Locale
 import java.util.concurrent.TimeUnit
@@ -38,18 +46,20 @@ class GalleryFragment internal constructor() : Fragment() {
     // Get root directory of media from navigation arguments
     var rootDirectory:File? = null
 
-    private val listOfEncryptedFilePaths = arrayListOf<String>()
-    private val listOfDecryptedFilePaths = arrayListOf<String>()
-    private val listOfImages = arrayListOf<ImageItem>()
+//    private val listOfEncryptedFilePaths = arrayListOf<String>()
+//    private val listOfDecryptedFilePaths = arrayListOf<String>()
+//    private val listOfImages = arrayListOf<ImageItem>()
 
-    lateinit var progressDialog:ProgressBar
-    lateinit var btn_decrypt:Button
+//    lateinit var progressDialog:ProgressBar
+//    lateinit var btn_decrypt:Button
     lateinit var cutout_safe_area:ConstraintLayout
+    lateinit var photo_view_pager:ViewPager
+    lateinit var btn_suggest_if_no_image:Button
   //  lateinit var photo_view_pager:ViewPager
    // var mediaViewPager:ViewPager?= null
-    private lateinit var layoutManager: StaggeredGridLayoutManager
-    private lateinit var recycler_view:RecyclerView
-    private lateinit var adapter: ImageAdapter
+//    private lateinit var layoutManager: StaggeredGridLayoutManager
+//    private lateinit var recycler_view:RecyclerView
+//    private lateinit var adapter: ImageAdapter
 
     private lateinit var mediaList: MutableList<File>
 
@@ -84,42 +94,46 @@ class GalleryFragment internal constructor() : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        progressDialog = view.findViewById(R.id.progressDialog)
-        btn_decrypt = view.findViewById(R.id.btn_decrypt)
+     //   progressDialog = view.findViewById(R.id.progressDialog)
+     //   btn_decrypt = view.findViewById(R.id.btn_decrypt)
         cutout_safe_area = view.findViewById(R.id.cutout_safe_area)
-        recycler_view = view.findViewById(R.id.recycler_view)
+        photo_view_pager = view.findViewById(R.id.photo_view_pager)
+        btn_suggest_if_no_image = view.findViewById(R.id.btn_suggest_if_no_image)
+    //    recycler_view = view.findViewById(R.id.recycler_view)
       //  photo_view_pager = view.findViewById(R.id.photo_view_pager)
 
-       encryptImages()
-        Log.d("gallery", listOfEncryptedFilePaths.toString())
-        Log.d("gallery", listOfImages.toString())
+      // encryptImages()
+//        Log.d("gallery", listOfEncryptedFilePaths.toString())
+//        Log.d("gallery", listOfImages.toString())
 
-        setUpListAdapter()
+       // setUpListAdapter()
 
         //Checking media files list
         if (mediaList.isEmpty()) {
             view.findViewById<ImageButton>(R.id.delete_button).isEnabled = false
             view.findViewById<ImageButton>(R.id.share_button).isEnabled = false
+            btn_suggest_if_no_image.visibility = View.VISIBLE
+
         }
 
-//        mediaViewPager = photo_view_pager.apply {
-//            offscreenPageLimit = 2
-//            adapter = MediaPagerAdapter(childFragmentManager)
+       val mediaViewPager = photo_view_pager.apply {
+            offscreenPageLimit = 2
+            adapter = MediaPagerAdapter(childFragmentManager)
+        }
+//
+//        btn_decrypt.setOnClickListener {
+//            Log.d("gallery", listOfDecryptedFilePaths.toString())
+//
+//            if (listOfEncryptedFilePaths.isNotEmpty()) {
+//                Log.d("gallery",listOfImages.toString())
+//                showHideProgress(true)
+//
+//                decryptImages()
+//
+//            } else {
+//                Toast.makeText(requireContext(), "There are no encrypted images!", Toast.LENGTH_SHORT).show()
+//            }
 //        }
-
-        btn_decrypt.setOnClickListener {
-            Log.d("gallery", listOfDecryptedFilePaths.toString())
-
-            if (listOfEncryptedFilePaths.isNotEmpty()) {
-                Log.d("gallery",listOfImages.toString())
-                showHideProgress(true)
-
-                decryptImages()
-
-            } else {
-                Toast.makeText(requireContext(), "There are no encrypted images!", Toast.LENGTH_SHORT).show()
-            }
-        }
 
         // Make sure that the cutout "safe area" avoids the screen notch if any
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
@@ -132,161 +146,161 @@ class GalleryFragment internal constructor() : Fragment() {
             Navigation.findNavController(requireActivity(), R.id.fragment_container).navigateUp()
         }
 
-        // Handle share button press
-//        view.findViewById<ImageButton>(R.id.share_button).setOnClickListener {
-//
-//            mediaViewPager?.currentItem?.let { it1 ->
-//                mediaList.getOrNull(it1)?.let { mediaFile ->
-//
-//                    // Create a sharing intent
-//                    val intent = Intent().apply {
-//                        // Infer media type from file extension
-//                        val mediaType = MimeTypeMap.getSingleton()
-//                            .getMimeTypeFromExtension(mediaFile.extension)
-//                        // Get URI from our FileProvider implementation
-//                        val uri = FileProvider.getUriForFile(
-//                            view.context, BuildConfig.APPLICATION_ID + ".provider", mediaFile)
-//                        // Set the appropriate intent extra, type, action and flags
-//                        putExtra(Intent.EXTRA_STREAM, uri)
-//                        type = mediaType
-//                        action = Intent.ACTION_SEND
-//                        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-//                    }
-//
-//                    // Launch the intent letting the user choose which app to share with
-//                    startActivity(Intent.createChooser(intent, getString(R.string.share_hint)))
-//                }
-//            }
-//        }
+      //   Handle share button press
+        view.findViewById<ImageButton>(R.id.share_button).setOnClickListener {
 
-        // Handle delete button press
-//        view.findViewById<ImageButton>(R.id.delete_button).setOnClickListener {
-//
-//            mediaViewPager?.currentItem?.let { it1 ->
-//                mediaList.getOrNull(it1)?.let { mediaFile ->
-//
-//                    AlertDialog.Builder(view.context, android.R.style.Theme_Material_Dialog)
-//                        .setTitle(getString(R.string.delete_title))
-//                        .setMessage(getString(R.string.delete_dialog))
-//                        .setIcon(android.R.drawable.ic_dialog_alert)
-//                        .setPositiveButton(android.R.string.yes) { _, _ ->
-//
-//                            // Delete current photo
-//                            mediaFile.delete()
-//
-//                            // Send relevant broadcast to notify other apps of deletion
-//                            MediaScannerConnection.scanFile(
-//                                view.context, arrayOf(mediaFile.absolutePath), null, null)
-//
-//                            // Notify our view pager
-//                            mediaList.removeAt(mediaViewPager!!.currentItem)
-//                            mediaViewPager!!.adapter?.notifyDataSetChanged()
-//
-//                            // If all photos have been deleted, return to camera
-//                            if (mediaList.isEmpty()) {
-//                                Navigation.findNavController(requireActivity(), R.id.fragment_container).navigateUp()
-//                            }
-//
-//                        }
-//
-//                        .setNegativeButton(android.R.string.no, null)
-//                        .create().showImmersive()
-//                }
-//            }
-//        }
+            mediaViewPager?.currentItem?.let { it1 ->
+                mediaList.getOrNull(it1)?.let { mediaFile ->
+
+                    // Create a sharing intent
+                    val intent = Intent().apply {
+                        // Infer media type from file extension
+                        val mediaType = MimeTypeMap.getSingleton()
+                            .getMimeTypeFromExtension(mediaFile.extension)
+                        // Get URI from our FileProvider implementation
+                        val uri = FileProvider.getUriForFile(
+                            view.context, BuildConfig.APPLICATION_ID + ".provider", mediaFile)
+                        // Set the appropriate intent extra, type, action and flags
+                        putExtra(Intent.EXTRA_STREAM, uri)
+                        type = mediaType
+                        action = Intent.ACTION_SEND
+                        flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                    }
+
+                    // Launch the intent letting the user choose which app to share with
+                    startActivity(Intent.createChooser(intent, getString(R.string.share_hint)))
+                }
+            }
+        }
+
+     //    Handle delete button press
+        view.findViewById<ImageButton>(R.id.delete_button).setOnClickListener {
+
+            mediaViewPager?.currentItem?.let { it1 ->
+                mediaList.getOrNull(it1)?.let { mediaFile ->
+
+                    androidx.appcompat.app.AlertDialog.Builder(view.context, android.R.style.Theme_Material_Dialog)
+                        .setTitle(getString(R.string.delete_title))
+                        .setMessage(getString(R.string.delete_dialog))
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes) { _, _ ->
+
+                            // Delete current photo
+                            mediaFile.delete()
+
+                            // Send relevant broadcast to notify other apps of deletion
+                            MediaScannerConnection.scanFile(
+                                view.context, arrayOf(mediaFile.absolutePath), null, null)
+
+                            // Notify our view pager
+                            mediaList.removeAt(mediaViewPager!!.currentItem)
+                            mediaViewPager!!.adapter?.notifyDataSetChanged()
+
+                            // If all photos have been deleted, return to camera
+                            if (mediaList.isEmpty()) {
+                                Navigation.findNavController(requireActivity(), R.id.fragment_container).navigateUp()
+                            }
+
+                        }
+
+                        .setNegativeButton(android.R.string.no, null)
+                        .create().showImmersive()
+                }
+            }
+        }
     }
 
     /*
     * Setup RecyclerView list adapter.
     */
-    private fun setUpListAdapter() {
-        adapter = ImageAdapter(requireContext(),listOfImages)
+//    private fun setUpListAdapter() {
+//        adapter = ImageAdapter(requireContext(),listOfImages)
+//
+//        layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+//        layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+//
+//        recycler_view.layoutManager = layoutManager
+//        recycler_view.adapter = adapter
+//    }
 
-        layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_NONE
+//    private fun encryptImages() {
+//
+//        showHideProgress(true)
+//
+//        val directory = File(rootDirectory?.toURI())
+//        val listOfPaths = arrayListOf<String>()
+//
+//        if (directory.exists()) {
+//
+//            //Get list of files in storage directory
+//            val list: Array<File> = directory.listFiles()
+//
+//            for (item in list) {
+//                listOfPaths.add(item.path)
+//            }
+//
+//            ImageCrypter.encryptImageList(listOfPaths)
+//                .delay(1, TimeUnit.SECONDS)
+//                .subscribeBy(
+//                    onNext = {
+//                        listOfEncryptedFilePaths.add(it.path)
+//                    },
+//                    onError = {
+//                        it.printStackTrace()
+//                        showHideProgress(false)
+//                        Toast.makeText(requireContext(), "Something went wrong.", Toast.LENGTH_SHORT).show()
+//                    },
+//                    onComplete = {
+//                        activity?.runOnUiThread {
+//                            Toast.makeText(
+//                                requireContext(),
+//                                "All image files are encrypted!",
+//                                Toast.LENGTH_SHORT
+//                            ).show()
+//                            btn_decrypt.visibility = View.VISIBLE
+//                            showHideProgress(false)
+//                        }
+//                    }
+//                )
+//        }
+//    }
 
-        recycler_view.layoutManager = layoutManager
-        recycler_view.adapter = adapter
-    }
-
-    private fun encryptImages() {
-
-        showHideProgress(true)
-
-        val directory = File(rootDirectory?.toURI())
-        val listOfPaths = arrayListOf<String>()
-
-        if (directory.exists()) {
-
-            //Get list of files in storage directory
-            val list: Array<File> = directory.listFiles()
-
-            for (item in list) {
-                listOfPaths.add(item.path)
-            }
-
-            ImageCrypter.encryptImageList(listOfPaths)
-                .delay(1, TimeUnit.SECONDS)
-                .subscribeBy(
-                    onNext = {
-                        listOfEncryptedFilePaths.add(it.path)
-                    },
-                    onError = {
-                        it.printStackTrace()
-                        showHideProgress(false)
-                        Toast.makeText(requireContext(), "Something went wrong.", Toast.LENGTH_SHORT).show()
-                    },
-                    onComplete = {
-                        activity?.runOnUiThread {
-                            Toast.makeText(
-                                requireContext(),
-                                "All image files are encrypted!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                            btn_decrypt.visibility = View.VISIBLE
-                            showHideProgress(false)
-                        }
-                    }
-                )
-        }
-    }
-
-    private fun decryptImages(){
-
-        ImageCrypter.decryptFiles(listOfEncryptedFilePaths)
-            .subscribeBy(
-                onNext = {
-                    it.forEach {
-                        listOfDecryptedFilePaths.add(it.path)
-                    }
-                },
-                onError = {
-                    it.printStackTrace()
-                    showHideProgress(false)
-                    Toast.makeText(requireContext(), "Something went wrong.", Toast.LENGTH_SHORT).show()
-                    btn_decrypt.visibility = View.VISIBLE
-                },
-                onComplete = {
-                    showHideProgress(false)
-                    btn_decrypt.visibility = View.GONE
-                    cutout_safe_area.visibility = View.VISIBLE
-
-                    //Get list of decrypted files
-                    for (item in listOfDecryptedFilePaths) {
-                        listOfImages.add(ImageItem(item))
-                    }
-
-                }
-            )
-    }
-
-    private fun showHideProgress(show: Boolean) {
-        if (show) {
-            progressDialog.visibility = View.VISIBLE
-        } else {
-            progressDialog.visibility = View.GONE
-        }
-    }
+//    private fun decryptImages(){
+//
+//        ImageCrypter.decryptFiles(listOfEncryptedFilePaths)
+//            .subscribeBy(
+//                onNext = {
+//                    it.forEach {
+//                        listOfDecryptedFilePaths.add(it.path)
+//                    }
+//                },
+//                onError = {
+//                    it.printStackTrace()
+//                    showHideProgress(false)
+//                    Toast.makeText(requireContext(), "Something went wrong.", Toast.LENGTH_SHORT).show()
+//                    btn_decrypt.visibility = View.VISIBLE
+//                },
+//                onComplete = {
+//                    showHideProgress(false)
+//                    btn_decrypt.visibility = View.GONE
+//                    cutout_safe_area.visibility = View.VISIBLE
+//
+//                    //Get list of decrypted files
+//                    for (item in listOfDecryptedFilePaths) {
+//                        listOfImages.add(ImageItem(item))
+//                    }
+//
+//                }
+//            )
+//    }
+//
+//    private fun showHideProgress(show: Boolean) {
+//        if (show) {
+//            progressDialog.visibility = View.VISIBLE
+//        } else {
+//            progressDialog.visibility = View.GONE
+//        }
+//    }
 
 
 }
